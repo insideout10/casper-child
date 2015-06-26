07843b8ea62a179ab1c848617fddef23e3898444
pieroit <savastanos@libero.it>
2015-06-26 16:32:25 +0200

    head fix

--- header.php
+++ header.php
@@ -38,7 +38,6 @@
         <?php wp_nav_menu( array( 'theme_location' => 'primary' ) ); ?>
     </nav> 
     
-    <br><br>
     <a class="icon-bars" id="mmenu-toggle">
         <span class="hidden"><?php _e( 'Menu', 'casper' ); ?></span>
     </a>
